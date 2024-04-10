79e1abbad18a120715b0721aa20088c60a0f9c23
Shanmuganathan S <shanmuganathans@Shanmuganathans-MacBook-Pro.local>
2024-04-10 21:40:41 +0530

    Class based views Detail, Update and Delete View

--- Class_Based_Views/class_views/Testing/urls.py
+++ Class_Based_Views/class_views/Testing/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path("cbv_create_view", views.MyCreateView.as_view(), name="cbv_create_view"),
     path("fbv_list_view", views.fbv_list_view, name="fbv_list_view"),
     path("cbv_list_view", views.MyListView.as_view(), name="cbv_list_view"),
+    path("fbv_detail_view/<int:id>",views.fbv_detail_view, name ="fbv_detail_view"),
+    path("cbv_detail_view/<int:pk>",views.CBVDetailView.as_view(), name="cbv_detail_view"),
+    path("fbv_update_view/<int:id>",views.fbv_update_view, name="fbv_update_view"),
+    path("cbv_update_view/<int:id>",views.CBVUpdateView.as_view(), name="cbv_update_view"),
+    path("fbv_delete_view/<int:id>",views.fbv_delete_view, name="fbv_delete_view"),
+    path("cbv_delete_view/<int:id>",views.CBVDeleteView.as_view(), name="cbv_delete_view"),
 ]
--- Class_Based_Views/class_views/Testing/views.py
+++ Class_Based_Views/class_views/Testing/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
-from django.views.generic.edit import FormView, CreateView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from . forms import ContactForm
 from . models import Contact
 
@@ -58,6 +59,67 @@ def fbv_list_view(request):
 
 
 class MyListView(ListView):
-    template_name = "Testing/list_template.html"
+    template_name = "Testing/list_template_class_based.html"
     queryset =  Contact.objects.all()
     context_object_name = "contacts"
+
+
+def fbv_detail_view(request, id):
+    try:
+        obj = Contact.objects.get(id=id)
+    except Contact.DoesNotExist:
+        return HttpResponse("Contact not found.!")
+    context = {"contact": obj}
+    return render(request,"Testing/detail_view.html", context)
+
+
+class CBVDetailView(DetailView):
+    template_name = "Testing/detail_view.html"
+    pk_url_kwarg = 'pk'
+    queryset = Contact.objects.all()
+    
+def fbv_update_view(request, id):
+    obj = get_object_or_404(Contact, id =id)
+    if request.method =="GET":
+        context = {
+            "form":ContactForm(instance=obj)
+        }
+        return render(request,"Testing/form_update.html", context)
+    elif request.method =="POST":
+        form = ContactForm(request.POST, instance= obj)
+        if form.is_valid():
+            form.save()
+            return redirect("cbv_list_view")
+        else:
+            return render(request, "Testing/form_update.html",context)
+        
+        
+class CBVUpdateView(UpdateView):
+    template_name = "Testing/form_update.html"
+    form_class = ContactForm
+    pk_url_kwarg = "id"
+    model = Contact
+    
+    def form_valid(self, form):
+        form.save()
+        return redirect("cbv_list_view")
+    
+def fbv_delete_view(request, id):
+    object = get_object_or_404(Contact, id =id)
+    if request.method =="GET":
+        context = {"contact": object}
+        return render(request, "Testing/delete_template.html", context)
+    if request.method == "POST":
+        object.delete()
+        return HttpResponse("Contact deleted successfully..!")
+    
+    
+class CBVDeleteView(DeleteView):
+    template_name = "Testing/delete_template.html"
+    pk_url_kwarg = "id"
+    queryset = Contact.objects.all()
+    
+    def form_valid(self, form):
+        self.object = self.get_object()
+        self.object.delete()
+        return redirect("cbv_list_view")
