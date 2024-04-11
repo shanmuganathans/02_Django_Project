8fef9115cb6ba935e3d259e40c2120fb213d40cb
Shanmuganathan S <shanmuganathans@Shanmuganathans-MacBook-Pro.local>
2024-04-11 21:26:30 +0530

    File Operation's

---
+++ file_operations/file_operations/file_upload_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("upload/",views.upload_file, name="upload_file" ),
+    path("success/", views.upload_success, name="upload_success"),
+    
+]
---
+++ file_operations/file_operations/file_upload_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,redirect
+from .forms import UploadFileForm
+
+# Create your views here.
+
+def upload_file(request):
+    if request.method =="GET":
+        context = {
+            "form": UploadFileForm()
+        }
+        return render(request,"file_upload_app/upload.html", context)
+    elif request.method =="POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("upload_success")
+        else:
+            form = UploadFileForm()
+            context = {
+                "form": UploadFileForm()
+            }
+            return render(request,"file_upload_app/upload.html", context)
+        
+def upload_success(request):
+    return render(request,"file_upload_app/upload_success.html")
+            
